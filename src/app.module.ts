3925900781eb7ae407617ea81a204cf54324948a
be-dev1 <be-dev1@labsquire.com>
2023-12-08 19:12:15 +0530

    SR-- Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StatsModule } from './stats/stats.module';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StatsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }), 
+    StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
