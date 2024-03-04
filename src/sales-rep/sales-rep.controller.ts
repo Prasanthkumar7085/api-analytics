483c7345ef00c00226335aa519184dc0cdcab7d4
sunilpalepu631 <sunil.p@orotron.com>
2024-03-04 18:13:09 +0530

    SP--ADDED case-types revenue

--- src/sales-rep/sales-rep.controller.ts
+++ src/sales-rep/sales-rep.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Query } from '@nestjs/common';
 import { SalesRepService } from './sales-rep.service';
 import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
 import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
 import { SOMETHING_WENT_WRONG, SUCCESS_FETCHED_SALES_REP } from 'src/constants/messageConstants';
+import * as fs from 'fs';
+
+
 
 @Controller({
   version: '1.0',
@@ -11,13 +14,15 @@ import { SOMETHING_WENT_WRONG, SUCCESS_FETCHED_SALES_REP } from 'src/constants/m
 export class SalesRepController {
   constructor(private readonly salesRepService: SalesRepService) { }
 
-  @Post('')
+  @Post()
   async getAllSalesRep(@Res() res: any) {
     try {
+      const data = await this.salesRepService.findAll()
 
       return res.status(200).json({
         success: true,
-        message: SUCCESS_FETCHED_SALES_REP
+        message: SUCCESS_FETCHED_SALES_REP,
+        data: data
       })
     } catch (err) {
       console.log({ err });
@@ -27,4 +32,62 @@ export class SalesRepController {
       })
     }
   }
+
+
+  @Post('case-types/:id')
+  async getOneSalesRep(
+    @Param('id') id: any, @Res() res: any,
+    @Query('start_date') start_date: Date,
+    @Query('end_date') end_date: Date,
+  ) {
+    try {
+
+      const { totalCounts, total } = await this.salesRepService.findOneVolume(id, start_date, end_date)
+      const revenueData = await this.salesRepService.findOneRevenue(id, start_date, end_date)
+
+      return res.status(200).json({
+        success: true,
+        message: 'case types wise count',
+        data: {
+          volume_data: { total, count: totalCounts },
+          revenue_data: { total: revenueData.total_amount, case_wise_revenue: revenueData.totalCaseTypeAmount }
+        }
+      })
+    } catch (err) {
+      console.log({ err });
+      return res.status(500).json({
+        success: false,
+        message: err.message || SOMETHING_WENT_WRONG
+      })
+    }
+  }
+
+
+
+
+  @Post('case-types/revenue/:id')
+  async getOneSalesRepDuration(
+    @Param('id') id: string,
+    @Query('start_date') start_date: any, //lets assume its jan 1st 2023
+    @Query('end_date') end_date: any, //lets assume its dec 31st 2023
+    @Res() res: any) {
+    try {
+
+      const data = await this.salesRepService.getOneSalesRepDuration(id, new Date(start_date), new Date(end_date))
+
+      return res.status(200).json({
+        success: true,
+        message: 'Successfully fetched data',
+        data: data
+      })
+
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: error || SOMETHING_WENT_WRONG
+      })
+    }
+  }
+
+
 }
--- src/sales-rep/sales-rep.service.ts
+++ src/sales-rep/sales-rep.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
 import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
+import * as fs from 'fs';
+
+
+
 
 @Injectable()
 export class SalesRepService {
@@ -8,19 +12,112 @@ export class SalesRepService {
     return 'This action adds a new salesRep';
   }
 
-  findAll() {
-    return `This action returns all salesRep`;
+  async findAll() {
+    // from Volume
+    const volumeResponse = fs.readFileSync('./VolumeStatsData.json', "utf-8");
+
+    const finalVolumeResp = JSON.parse(volumeResponse);
+
+    return finalVolumeResp
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} salesRep`;
+
+
+  async findOneVolume(id: string, start_date: Date, end_date: Date) {
+    const volumeResponse = fs.readFileSync('./VolumeStatsData.json', "utf-8");
+    const finalVolumeResp = JSON.parse(volumeResponse);
+
+    let totalCounts = {};
+    let total: number;
+
+    for (let i = 0; i < finalVolumeResp.length; i++) {
+      if (finalVolumeResp[i].marketer_id == id) {
+        let date = finalVolumeResp[i].date
+        if (date >= start_date && date <= end_date) {
+
+          total = finalVolumeResp[i].total_cases
+
+          finalVolumeResp[i].case_type_wise_counts.forEach(caseType => {
+            const { case_type, pending, completed } = caseType;
+            totalCounts[case_type] = (totalCounts[case_type] || 0) + pending + completed;
+          });
+
+        }
+      }
+
+    }
+    return { totalCounts, total }
   }
 
-  update(id: number, updateSalesRepDto: UpdateSalesRepDto) {
-    return `This action updates a #${id} salesRep`;
+
+
+
+
+  async findOneRevenue(id: string, start_date: Date, end_date: Date) {
+
+    const revenueResponse = fs.readFileSync('./RevenueStatsData.json', "utf-8");
+    const revenue = JSON.parse(revenueResponse);
+
+    let totalCaseTypeAmount = {};
+    let total_amount: number;
+
+
+    for (const item of revenue) {
+      if (item.marketer_id == id) {
+        if (item.date >= start_date && item.date <= end_date) {
+
+
+
+          total_amount = item.total_amount
+
+          item.case_type_wise_counts.forEach(caseType => {
+            const { case_type, total_amount } = caseType;
+            totalCaseTypeAmount[case_type] = (totalCaseTypeAmount[case_type] || 0) + total_amount
+          })
+        }
+      }
+    }
+    return { totalCaseTypeAmount, total_amount }
+    // throw new NotFoundException('Revenue data not found for the specified marketer ID');
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} salesRep`;
+
+
+  async getOneSalesRepDuration(id, start_date, end_date) {
+    const revenueResponse = fs.readFileSync('./RevenueStatsData.json', "utf-8");
+    const revenue = JSON.parse(revenueResponse);
+
+
+    let total_counts = {}
+    const startDate = new Date(start_date)
+    const endDate = new Date(end_date)
+
+    while (startDate <= endDate) {
+      const monthYear = startDate.toLocaleString('default', { month: 'long', year: 'numeric' });
+
+      total_counts[monthYear] = {
+        total_revenue: 0,
+        case_type_wise: {}
+      };
+      startDate.setMonth(startDate.getMonth() + 1);
+    }
+
+    for (const item of revenue) {
+      if (item.marketer_id == id) {
+
+        let date = new Date(item.date);
+
+        if (date >= start_date && date <= end_date) {
+          const monthYear = date.toLocaleString('default', { month: 'long', year: 'numeric' })
+          item.case_type_wise_counts.forEach(caseType => {
+            const { case_type, total_amount } = caseType;
+
+            total_counts[monthYear]['case_type_wise'][case_type] = (total_counts[monthYear]['case_type_wise'][case_type] || 0) + total_amount
+            total_counts[monthYear]['total_revenue'] += total_amount
+          })
+        }
+      }
+    }
+    return total_counts
   }
 }
