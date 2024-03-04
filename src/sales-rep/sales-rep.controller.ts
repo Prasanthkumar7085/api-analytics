66d2ef9659f1a782abb51778fdae40fdbce88bce
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-04 11:58:37 +0530

    SR-- Updated sales rep controller

--- src/sales-rep/sales-rep.controller.ts
+++ src/sales-rep/sales-rep.controller.ts
@@ -1,34 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { SalesRepService } from './sales-rep.service';
 import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
 import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
+import { SOMETHING_WENT_WRONG, SUCCESS_FETCHED_SALES_REP } from 'src/constants/messageConstants';
 
-@Controller('sales-rep')
+@Controller({
+  version: '1.0',
+  path: 'sales-rep',
+})
 export class SalesRepController {
-  constructor(private readonly salesRepService: SalesRepService) {}
+  constructor(private readonly salesRepService: SalesRepService) { }
 
-  @Post()
-  create(@Body() createSalesRepDto: CreateSalesRepDto) {
-    return this.salesRepService.create(createSalesRepDto);
-  }
+  @Post('')
+  async getAllSalesRep(@Res() res: any) {
+    try {
 
-  @Get()
-  findAll() {
-    return this.salesRepService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.salesRepService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSalesRepDto: UpdateSalesRepDto) {
-    return this.salesRepService.update(+id, updateSalesRepDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.salesRepService.remove(+id);
+      return res.status(200).json({
+        success: true,
+        message: SUCCESS_FETCHED_SALES_REP
+      })
+    } catch (err) {
+      console.log({ err });
+      return res.status(500).json({
+        success: false,
+        message: err || SOMETHING_WENT_WRONG
+      })
+    }
   }
 }
