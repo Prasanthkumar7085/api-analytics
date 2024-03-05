f3e11719eb499e519f955017ad10ebb9aca87275
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-05 14:12:39 +0530

    SR-- Updated facility wise volume trend api

---
+++ src/facilities/dto/facility.dto.ts
@@ -0,0 +1,19 @@
+import { IsDateString, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class FacilityDto {
+
+    @IsMongoId({each: true, message: "Invalid Facility"})
+    @IsString({message: "Invalid Facility"})
+    @IsNotEmpty({message: "Facility is Required"})
+    hospital_id: string
+
+    @IsDateString({}, { message: "Invalid From Date" })
+    // @IsNotEmpty({message: "From Date is Required"})
+    @IsOptional()
+    from_date: Date;
+
+    @IsDateString({}, { message: "Invalid To Date" })
+    // @IsNotEmpty({message: "To Date is Required"})
+    @IsOptional()
+    to_date: Date;
+}
