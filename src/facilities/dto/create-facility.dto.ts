cc7fd3f2771d6a077adab61427e77fee799aaf2d
dev1_labsquire <be-dev1@labsquire.com>
2024-03-05 11:42:37 +0530

    SP--ADDED based on facility, case type wise  revenue api

--- src/facilities/dto/create-facility.dto.ts
+++ src/facilities/dto/create-facility.dto.ts
@@ -1,1 +1,27 @@
-export class CreateFacilityDto {}
+import { IsDateString, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+
+export class CreateFacilityDto {
+    @IsMongoId({ each: true, message: "Invalid Facility" })
+    @IsString({ message: "Invalid Facility" })
+    @IsNotEmpty({ message: "Facility is Required" })
+    facility_id: string
+
+    @IsDateString({}, { message: "Invalid From Date" })
+    @IsNotEmpty({ message: "From Date is Required" })
+    // @IsOptional()
+    from_date: Date;
+
+    @IsDateString({}, { message: "Invalid To Date" })
+    @IsNotEmpty({ message: "To Date is Required" })
+    // @IsOptional()
+    to_date: Date;
+
+    @IsString({ message: "Invalid Order By" })
+    @IsOptional()
+    order_by: string;
+
+    @IsString({ message: "Invalid Order Type" })
+    @IsOptional()
+    order_type: string;
+}
