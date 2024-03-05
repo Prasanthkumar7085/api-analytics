54c00ac80bc34d743cc6d5374a2a3c823e393c97
dev1_labsquire <be-dev1@labsquire.com>
2024-03-05 11:42:36 +0530

    RS-- Added facility wise stats revenue api

--- src/facilities/dto/create-facility.dto.ts
+++ src/facilities/dto/create-facility.dto.ts
@@ -1,1 +1,20 @@
-export class CreateFacilityDto {}
+import { IsDateString, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+
+export class CreateFacilityDto {
+    @IsMongoId({ each: true, message: "Invalid Marketer" })
+    @IsString({ message: "Invalid Marketer" })
+    @IsNotEmpty({ message: "Marketer is Required" })
+    facility_id: string
+
+    @IsDateString({}, { message: "Invalid From Date" })
+    @IsNotEmpty({message: "From Date is Required"})
+    // @IsOptional()
+    from_date: Date;
+
+    @IsDateString({}, { message: "Invalid To Date" })
+    @IsNotEmpty({message: "To Date is Required"})
+    // @IsOptional()
+    to_date: Date;
+
+}
