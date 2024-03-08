ef3804ab43bee27b19ec0e96524b5d5036148716
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-08 11:24:47 +0530

    SR-- Updated seeder

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -1,3 +1,15 @@
 import roles from "./roles";
+import insurance from "./insurancePayors"
+import salesRep from "./salesReps"
+import facilities from "./facilities"
 
-roles.seed("Roles");
+// roles.seed("Roles");
+
+// insurance.insuranceSeeder("Insurance")
+
+// salesRep.seed("Sales Reps")
+
+facilities.seed("Facilities")
+
+
+
