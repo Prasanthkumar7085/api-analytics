539604e5235eee6180d908a8bc79f63073d85027
sunilpalepu631 <sunil.p@orotron.com>
2024-03-08 18:23:57 +0530

    SP-- Added insurance-payors & stats-revenue apis

--- src/sales-rep/sales-rep.controller.ts
+++ src/sales-rep/sales-rep.controller.ts
@@ -1,34 +1,122 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, NotFoundException, Query } from '@nestjs/common';
 import { SalesRepService } from './sales-rep.service';
 import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
 import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
 
-@Controller('sales-rep')
+@Controller({ version: '3.0', path: 'sales-reps' })
 export class SalesRepController {
-  constructor(private readonly salesRepService: SalesRepService) {}
-
-  @Post()
-  create(@Body() createSalesRepDto: CreateSalesRepDto) {
-    return this.salesRepService.create(createSalesRepDto);
-  }
+  constructor(private readonly salesRepService: SalesRepService) { }
 
   @Get()
-  findAll() {
-    return this.salesRepService.findAll();
+  async getSalesRepStatsRevenue(
+    @Res() res: any
+  ) {
+    try {
+      const data = await this.salesRepService.getSalesRep();
+
+      return res.status(200).json(data)
+
+    }
+    catch (error) {
+
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  }
+  @Get('insurance')
+  async getInsurance(
+    @Res() res: any
+  ) {
+    try {
+      const data = await this.salesRepService.getInsurance();
+
+      return res.status(200).json(data)
+
+    }
+    catch (error) {
+
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  }
+  @Get('facilities')
+  async getFacilities(
+    @Res() res: any
+  ) {
+    try {
+
+      const data = await this.salesRepService.getFacilities();
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  }
+  @Get('case-types')
+  async getCaseTypes(
+    @Res() res: any
+  ) {
+    try {
+
+      const data = await this.salesRepService.getCaseTypes();
+      return res.status(200).json(data)
+
+
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message })
+    }
+  }
+  @Get('patient-claims')
+  async getSalesRepPatientClaims(
+    @Res() res: any
+  ) {
+    try {
+
+      const data = await this.salesRepService.getSalesRepPatientClaims();
+      return res.status(200).json(data)
+
+
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message })
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.salesRepService.findOne(+id);
+
+
+  @Get(':id/stats-revenue')
+  async getStatsRevenue(@Res() res: any, @Param('id') id: any, @Query() query: any) {
+    try {
+      const data = await this.salesRepService.getStatsRevenue(id, query)
+
+      return res.status(200).json({ success: true, message: 'Stats revenue Fetched Successfully', data: data })
+    }
+    catch (error) {
+      if (error instanceof NotFoundException) {
+        return res.status(404).json({ success: false, message: error.message })
+
+      }
+      return res.status(500).json({ success: false, error: error.message })
+    }
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSalesRepDto: UpdateSalesRepDto) {
-    return this.salesRepService.update(+id, updateSalesRepDto);
+
+
+
+  @Get(':id/insurance-payors')
+  async getInsurancePayers(
+    @Res() res: any, @Param('id') id: number, @Query() query: any
+  ) {
+    try {
+
+      const data = await this.salesRepService.getInsurancePayers(id, query)
+
+      return res.status(200).json({ success: true, message: 'Insurance payers data Fetched Successfully', data: data })
+
+    }
+    catch (error) {
+      if (error instanceof NotFoundException) {
+        return res.status(404).json({ success: false, message: error.message })
+
+      }
+      return res.status(500).json({ success: false, error: error.message })
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.salesRepService.remove(+id);
-  }
 }
--- src/sales-rep/sales-rep.service.ts
+++ src/sales-rep/sales-rep.service.ts
@@ -1,26 +1,167 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
 import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
+import { sales_reps } from 'src/drizzle/schemas/salesReps';
+import { db } from 'src/seeders/db';
+import { facilities } from 'src/drizzle/schemas/facilities';
+import { case_types } from 'src/drizzle/schemas/caseTypes';
+import { patient_claims } from 'src/drizzle/schemas/patientClaims';
+import { sql } from 'drizzle-orm';
+import { insurance_payors } from 'src/drizzle/schemas/insurancePayors';
+import insurancePayors from 'src/seeders/insurancePayors';
 
 @Injectable()
 export class SalesRepService {
-  create(createSalesRepDto: CreateSalesRepDto) {
-    return 'This action adds a new salesRep';
+
+  async getInsurance() {
+    const data = await db.select().from(insurance_payors)
+
+    return data
   }
 
-  findAll() {
-    return `This action returns all salesRep`;
+
+  async getSalesRep() {
+    const sales = await db.select().from(sales_reps)
+
+    console.log(sales)
+    return sales
+  }
+  async getFacilities() {
+    const sales = await db.select().from(facilities)
+
+    console.log(sales)
+    return sales
+  }
+  async getCaseTypes() {
+    const sales = await db.select().from(case_types)
+
+    console.log(sales)
+    return sales
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} salesRep`;
+
+  async getSalesRepStatsVolume() {
+    const sales = await db.select().from(sales_reps)
+
+    console.log(sales)
+    return sales
+  }
+  async getSalesRepPatientClaims() {
+    const sales = await db.select().from(patient_claims)
+
+    console.log(sales)
+    return sales
   }
 
-  update(id: number, updateSalesRepDto: UpdateSalesRepDto) {
-    return `This action updates a #${id} salesRep`;
+
+  async getStatsRevenue(id, query) {
+    // Write raw SQL query to join and calculate totals
+    let { from_date, to_date } = query;
+
+    // Ensure that from_date and to_date are valid dates, if provided
+    if (from_date) from_date = new Date(from_date);
+    if (to_date) to_date = new Date(to_date);
+
+
+    // Check if the provided id exists in the database
+    // const salesRepExists = await db.execute(sql`
+    //     SELECT COUNT(*) AS count
+    //     FROM ${sales_reps}
+    //     WHERE id = ${id}
+    // `);
+
+    // if (salesRepExists.rows.length === 0) {
+    //   throw new NotFoundException('Sales Representative not found');
+    // }
+
+
+    // Build the SQL query with optional date filter
+    let statement = sql`
+      SELECT 
+        ROUND(SUM(${patient_claims.billableAmount})::NUMERIC, 2) AS billed,
+        ROUND(SUM(${patient_claims.clearedAmount})::NUMERIC, 2) AS collected,
+        ROUND(SUM(${patient_claims.pendingAmount})::NUMERIC, 2) AS pending
+      FROM 
+        ${sales_reps}
+      LEFT JOIN 
+        ${patient_claims} ON ${sales_reps.id} = ${patient_claims.salesRepId}
+      WHERE 
+        ${sales_reps.id} = ${id}
+    `;
+
+    // Conditionally add the date filter if both from_date and to_date are provided
+    if (from_date && to_date) {
+      statement.append(sql`
+        AND ${patient_claims.serviceDate} BETWEEN ${from_date} AND ${to_date}
+      `);
+    }
+
+    statement.append(sql`
+      GROUP BY 
+        ${sales_reps.id};
+    `);
+
+    // Execute the raw SQL query
+    const data = await db.execute(statement);
+
+    return data.rows[0];
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} salesRep`;
+
+
+  async getInsurancePayers(id, query) {
+
+    let { from_date, to_date } = query;
+
+    if (from_date) from_date = new Date(from_date);
+    if (to_date) to_date = new Date(to_date);
+
+
+    // const salesRepExists = await db.execute(sql`
+    //     SELECT COUNT(*) AS count
+    //     FROM ${sales_reps}
+    //     WHERE id = ${id}
+    // `);
+    // console.log({ salesRepExists })
+    // if (salesRepExists.rows.length === 0) {
+    //   throw new NotFoundException('Sales Representative not found');
+    // }
+
+
+    let statement = sql`
+    SELECT 
+        ${insurance_payors.name} AS insurance_name,
+        ROUND(SUM(${patient_claims.billableAmount})::NUMERIC, 2) AS total,
+        ROUND(SUM(${patient_claims.clearedAmount})::NUMERIC, 2) AS paid,
+        ROUND(SUM(${patient_claims.pendingAmount})::NUMERIC, 2) AS pending
+    FROM ${sales_reps}
+    JOIN ${patient_claims} ON ${patient_claims.salesRepId} = ${sales_reps.id}
+    JOIN ${insurance_payors} ON ${patient_claims.insurancePayerId} = ${insurance_payors.id}
+    WHERE ${sales_reps.id} = ${id}
+
+    
+    `;
+
+
+    // Conditionally add the date filter if both from_date and to_date are provided
+    if (from_date && to_date) {
+      statement.append(sql`
+        AND ${patient_claims.serviceDate} BETWEEN ${from_date} AND ${to_date}
+      `);
+    }
+
+    statement.append(sql`
+      GROUP BY 
+        ${insurance_payors.name};
+    `);
+
+    // Execute the raw SQL query
+    const data = await db.execute(statement);
+
+
+    return data.rows;
+
+
   }
+
 }
