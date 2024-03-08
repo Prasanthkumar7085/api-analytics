06864489120b2471688f5747641d74f5c4aa8eb4
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-08 18:47:25 +0530

    RS-- Updated sales trends

--- src/sales-rep/sales-rep.service.ts
+++ src/sales-rep/sales-rep.service.ts
@@ -1,34 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
-import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
 import { db } from 'src/seeders/db';
-import { sales_reps } from 'src/drizzle/schemas/salesReps';
-import { patient_claims } from 'src/drizzle/schemas/patientClaims';
-import { eq, sql } from 'drizzle-orm';
+import { sql } from 'drizzle-orm';
 
 @Injectable()
 export class SalesRepService {
-  // salesReps = db.select().from(sales_reps)
-
-
-  // async getCaseTypes(id:any){
-  //   const data = await db.select().from(patient_claims).where(eq(patient_claims.salesRepId, id))
-  //   const resultArray = []
-    
-  //   data.forEach(item => {
-  //     const case_type = item.caseTypeId
-  //     const revenue = item.clearedAmount
-
-  //     const existingCaseType = resultArray.find(caseItem => caseItem.case_type === case_type);
-  //     if (existingCaseType){
-  //       existingCaseType.revenue += revenue
-  //     }else{
-  //       resultArray.push({case_type:case_type, revenue:revenue})
-  //     }
-
-  //   })
-  //   return resultArray
-  // }
 
   async getCaseTypes(id:any){
     const query = sql`
