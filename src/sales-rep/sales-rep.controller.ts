ae0cc484290fad9582fcb9c3cc4691ca1c57647b
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-08 18:46:02 +0530

    RS-- Added sales trends revenue and volume api

--- src/sales-rep/sales-rep.controller.ts
+++ src/sales-rep/sales-rep.controller.ts
@@ -1,34 +1,69 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, Query } from '@nestjs/common';
 import { SalesRepService } from './sales-rep.service';
-import { CreateSalesRepDto } from './dto/create-sales-rep.dto';
-import { UpdateSalesRepDto } from './dto/update-sales-rep.dto';
 
-@Controller('sales-rep')
+
+@Controller({
+  version:'3.0',
+  path:'sales-reps'
+})
 export class SalesRepController {
   constructor(private readonly salesRepService: SalesRepService) {}
 
-  @Post()
-  create(@Body() createSalesRepDto: CreateSalesRepDto) {
-    return this.salesRepService.create(createSalesRepDto);
+
+  @Get('case-types/:id')
+  async getCaseTypes(@Res() res:any, @Param() param:any){
+    try {
+      const salesRespId = param.id
+      const data = await this.salesRepService.getCaseTypes(salesRespId)
+      return res.status(200).json({
+        sucess:true,
+        data:data
+      })
+    }
+    catch (err) {
+      return res.status(500).json({
+        success:true,
+        message:err
+      })
+    }
   }
 
-  @Get()
-  findAll() {
-    return this.salesRepService.findAll();
+  @Get(':id/trends/revenue')
+  async getTrendsRevenue(@Res() res:any, @Param() param:any, @Query() query:any){
+    try {
+      const id = param.id
+      const fromDate = query.from_date
+      const toDate = query.to_date
+      const data = await this.salesRepService.getTrendsRevenue(id,fromDate,toDate)
+      return res.status(200).json({
+        sucess:true,
+        data:data
+      })
+    } catch (err) {
+      return res.status(500).json({
+        success:true,
+        message:err
+      })
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.salesRepService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSalesRepDto: UpdateSalesRepDto) {
-    return this.salesRepService.update(+id, updateSalesRepDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.salesRepService.remove(+id);
+  @Get(':id/trends/volume')
+  async getTrendsVolume(@Res() res:any, @Param() param:any, @Query() query:any){
+    try {
+      const id = param.id
+      const fromDate = query.from_date
+      const toDate = query.to_date
+      const data = await this.salesRepService.getTrendsVolume(id,fromDate,toDate)
+      return res.status(200).json({
+        sucess:true,
+        data:data
+      })
+    } catch (err) {
+      return res.status(500).json({
+        success:true,
+        message:err
+      })
+    }
   }
 }
