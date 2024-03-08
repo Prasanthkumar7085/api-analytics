9fa58e32f231625c65386e2421b213768dae8ecc
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-08 18:27:39 +0530

    SR-- Updated sales reps apis

--- src/sales-rep/sales-rep.module.ts
+++ src/sales-rep/sales-rep.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SalesRepService } from './sales-rep.service';
 import { SalesRepController } from './sales-rep.controller';
+import { FilterHelper } from 'src/helpers/filterHelper';
 
 @Module({
   controllers: [SalesRepController],
-  providers: [SalesRepService],
+  providers: [SalesRepService, FilterHelper],
 })
 export class SalesRepModule {}
