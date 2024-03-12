cfef372ecd10423a17aa1e3dd161445304c86b64
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-12 20:07:53 +0530

    SR-- Updated changer

---
+++ gitchanger.sh
@@ -0,0 +1,11 @@
+git filter-branch -f --commit-filter '
+        if [ "$GIT_COMMITTER_EMAIL" = "roshan.sk@orotron.com" ];
+        then
+                GIT_COMMITTER_NAME="dev1_labsquire";
+                GIT_AUTHOR_NAME="dev1_labsquire";
+                GIT_COMMITTER_EMAIL="be-dev1@labsquire.com";
+                GIT_AUTHOR_EMAIL="be-dev1@labsquire.com";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
