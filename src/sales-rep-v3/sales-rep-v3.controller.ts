df7ed094982c0d6fa37b641e8c060c78f7245d5b
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-15 16:21:19 +0530

    SP-- Updated sales-rep controller and formatted code

--- src/sales-rep-v3/sales-rep-v3.controller.ts
+++ src/sales-rep-v3/sales-rep-v3.controller.ts
@@ -29,10 +29,83 @@ export class SalesRepControllerV3 {
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP,
 				data: salesReps
-			})
+			});
 		}
 		catch (error) {
 			console.log({ error });
+
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
+		}
+	}
+
+
+	@Get(':id')
+	async getOne(@Param('id') id: any, @Res() res: any) {
+		try {
+
+			const data = await this.salesRepService.getOne(id);
+
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_FETCHED_ONE_SALES_REP,
+				data: data
+			});
+		}
+		catch (error) {
+			console.log({ error });
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
+		}
+	}
+
+
+	@Get(':id/stats-revenue')
+	async getRevenueStats(@Res() res: any, @Param('id') id: any, @Query() query: any) {
+		try {
+
+			const queryString = await this.filterHelper.salesRep(query);
+
+			const data = await this.salesRepService.getStatsRevenue(id, queryString)
+
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_FECTED_SALE_REP_REVENUE_STATS,
+				data: data
+			});
+		}
+		catch (error) {
+			console.log({ error });
+
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
+		}
+	}
+
+
+	@Get(':id/stats-volume')
+	async getVolumeStats(@Res() res: any, @Param('id') id: any, @Query() query: any) {
+		try {
+
+			const queryString = this.filterHelper.salesRep(query);
+
+			const data = await this.salesRepService.getStatsVolume(id, queryString);
+
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_FECTED_SALE_REP_VOLUME_STATS,
+				data: data
+			});
+		}
+		catch (error) {
+			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
@@ -42,226 +115,224 @@ export class SalesRepControllerV3 {
 
 
 	@Get(':id/case-types')
-	async getOverallCaseTypes(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getOverallCaseTypes(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
 
-			const id = param.id;
 			const queryString = this.filterHelper.salesRep(query);
+
 			const salesReps = await this.salesRepService.getOverAllCaseTypes(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP_VOLUME_AND_REVENUE,
 				data: salesReps
-			})
-		} catch (error) {
+			});
+		}
+		catch (error) {
 			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
+			});
 		}
 	}
 
+
 	@Get(':id/case-types/revenue')
-	async getCaseTypesRevenue(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getCaseTypesRevenue(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
-			const id = param.id;
+
 			const queryString = this.filterHelper.salesRep(query);
 
 			const salesReps = await this.salesRepService.getCaseTypesRevenue(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_CASE_TYPES_REVENUE,
 				data: salesReps
-			})
-		} catch (error) {
+			});
+
+		}
+		catch (error) {
 			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
+			});
 		}
 	}
 
 
 	@Get(':id/case-types/volume')
-	async getCaseTypesVolume(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getCaseTypesVolume(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
-			const id = param.id;
+
 			const queryString = this.filterHelper.salesRep(query);
 
 			const salesReps = await this.salesRepService.getCaseTypesVolume(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP_CASE_TYPE_MONTHLY_VOLUME,
 				data: salesReps
-			})
-		} catch (error) {
+			});
+		}
+		catch (error) {
 			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
+			});
+		}
+	}
+
+
+	@Get(':id/insurance-payors')
+	async getInsurancePayers(@Res() res: any, @Param('id') id: any, @Query() query: any) {
+		try {
+
+			const queryString = this.filterHelper.salesRep(query);
+			const data = await this.salesRepService.getInsurancePayers(id, queryString);
+
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_FETCHED_SALES_REP_INSURANCE_PAYORS_DATA,
+				data: data
+			});
+		}
+		catch (error) {
+			console.log({ error });
+
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
 		}
 	}
 
 
 	@Get(':id/facilities')
-	async getFacilityWise(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getFacility(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
-			const id = param.id;
+
 			const queryString = this.filterHelper.salesRep(query);
 
 			const salesReps = await this.salesRepService.getFacilityWise(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP_FACILITY_WISE_STATS,
 				data: salesReps
-			})
-		} catch (error) {
+			});
+		}
+		catch (error) {
 			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
-		}
-	}
-
-	@Get(':id/stats-revenue')
-	async getStatsRevenue(@Res() res: any, @Param('id') id: any, @Query() query: any) {
-		try {
-
-			const queryString = await this.filterHelper.salesRep(query);
-
-			const data = await this.salesRepService.getStatsRevenue(id, queryString)
-
-			return res.status(200).json({ success: true, message: SUCCESS_FECTED_SALE_REP_REVENUE_STATS, data: data })
-		}
-		catch (error) {
-			console.log(error)
-			return res.status(500).json({ success: false, message: error || SOMETHING_WENT_WRONG })
-		}
-	}
-
-
-
-
-	@Get(':id/insurance-payors')
-	async getInsurancePayers(
-		@Res() res: any, @Param('id') id: number, @Query() query: any
-	) {
-		try {
-
-			const queryString = this.filterHelper.salesRep(query);
-			const data = await this.salesRepService.getInsurancePayers(id, queryString)
-
-			return res.status(200).json({ success: true, message: SUCCESS_FETCHED_SALES_REP_INSURANCE_PAYORS_DATA, data: data })
-
-		}
-		catch (error) {
-			console.log({ error });
-
-			return res.status(500).json({ success: false, message: error || SOMETHING_WENT_WRONG })
+			});
 		}
 	}
 
 
 	@Get(':id/trends/revenue')
-	async getTrendsRevenue(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getRevenueTrends(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
-			const id = param.id
+
 			const queryString = this.filterHelper.salesRep(query);
-			const data = await this.salesRepService.getTrendsRevenue(id, queryString)
+
+			const data = await this.salesRepService.getTrendsRevenue(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP_TREND_REVENUE,
 				data: data
-			})
-		} catch (error) {
+			});
+		}
+		catch (error) {
+			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
+			});
 		}
 	}
 
+
 	@Get(':id/trends/volume')
-	async getTrendsVolume(@Res() res: any, @Param() param: any, @Query() query: any) {
+	async getVolumeTrends(@Res() res: any, @Param('id') id: any, @Query() query: any) {
 		try {
-			const id = param.id
+
 			const queryString = this.filterHelper.salesRep(query);
-			const data = await this.salesRepService.getTrendsVolume(id, queryString)
+
+			const data = await this.salesRepService.getTrendsVolume(id, queryString);
+
 			return res.status(200).json({
 				success: true,
 				message: SUCCESS_FETCHED_SALES_REP_TREND_VOLUME,
 				data: data
-			})
-		} catch (error) {
-			console.log(error)
+			});
+		}
+		catch (error) {
+			console.log({ error });
+
 			return res.status(500).json({
 				success: false,
 				message: error || SOMETHING_WENT_WRONG
-			})
+			});
 		}
 	}
 
-	@Get(':id/stats-volume')
-	async getStatsVolume(@Res() res: any, @Param() param: any, @Query() query: any) {
-		try {
-			const id = param.id
-			const queryString = this.filterHelper.salesRep(query);
-			const data = await this.salesRepService.getStatsVolume(id, queryString)
-			return res.status(200).json({
-				success: true,
-				message: SUCCESS_FECTED_SALE_REP_VOLUME_STATS,
-				data: data
-			})
-		} catch (error) {
-			console.log(error)
-			return res.status(500).json({
-				success: false,
-				message: error || SOMETHING_WENT_WRONG
-			})
-		}
-	}
-
-	@Get(':id')
-	async getOne(@Param('id') id: any, @Res() res: any) {
-
-		try {
-			const data = await this.salesRepService.getOne(id)
-
-			return res.status(200).json({ success: true, message: SUCCESS_FETCHED_ONE_SALES_REP, data: data })
-
-		} catch (error) {
-			console.log(error.message)
-			return res.status(500).json({ success: false, message: error || SOMETHING_WENT_WRONG })
-		}
-	}
 
 	@Delete('delete')
 	async dropTable(@Res() res: any) {
 		try {
+
 			const data = await this.salesRepService.dropTable();
-			return res.status(200).json({ success: true, message: SUCCESS_DELETED_DATA_IN_TABLE, data: data });
+
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_DELETED_DATA_IN_TABLE,
+				data: data
+			});
 		}
 		catch (error) {
-			return res.status(500).json({ success: false, message: error || SOMETHING_WENT_WRONG })
+			console.log({ error });
+
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
 		}
 	}
+
 
 	@Get('patient-claims')
 	async getPatientClaims(@Query() query: any, @Res() res: any) {
 		try {
+
 			const queryString = this.filterHelper.salesRep(query);
 
 			const data = await this.salesRepService.getPatientClaims(queryString);
 
-			return res.status(200).json({ success: true, message: SUCCESS_FETCHED_PATIENT_CLAIMS_COUNT, data: data })
-
+			return res.status(200).json({
+				success: true,
+				message: SUCCESS_FETCHED_PATIENT_CLAIMS_COUNT,
+				data: data
+			});
 		}
 		catch (error) {
-			return res.status(500).json({ success: false, message: error || SOMETHING_WENT_WRONG })
+			console.log({ error });
+
+			return res.status(500).json({
+				success: false,
+				message: error || SOMETHING_WENT_WRONG
+			});
 		}
 	}
 
