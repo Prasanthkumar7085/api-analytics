39f3087d3e7b47f21304db2106f33cb8299d6935
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-25 16:38:26 +0530

    RS-- Added sales reps seeder

--- src/seederDev/salesReps.ts
+++ src/seederDev/salesReps.ts
@@ -8,31 +8,37 @@ import { sql } from "drizzle-orm";
 
 
 async function seedSalesReps() {
-    const configuration =  new Configuration(new ConfigService());
+    try {
 
-    const config = configuration.getConfig()
+        const configuration =  new Configuration(new ConfigService());
+        
+        const config = configuration.getConfig();
 
-    const dataBase = mongoose.connect(config)
+        const dataBase = mongoose.connect(config);
 
-    const data = await UserModel.find({ user_type : "HOSPITAL_MARKETING_MANAGER"})
+        // fetching data from mongoose db of LIS data
+        const data = await UserModel.find({ user_type : "HOSPITAL_MARKETING_MANAGER"})
 
-    const result_data = []
+        const result_data = [];
 
-    data.forEach(item => 
-        result_data.push(
-            {
-                name:item.first_name,
-                roleId:2,
-                refId:item._id.toString()
-            })
-        )
-    // console.log(result_data.length)
-
-    const salesRepsData = await db.insert(sales_reps).values(result_data).returning()
-
-    await mongoose.disconnect()
-
-    return salesRepsData
+        data.forEach(item => result_data.push(
+                        {
+                            name:item.first_name,
+                            roleId:2,
+                            refId:item._id.toString()
+                        })
+                    )
+            
+        const salesRepsData = await db.insert(sales_reps).values(result_data).returning();
+    
+        await mongoose.disconnect();
+    
+        return salesRepsData;
+    }
+    catch (error) {
+        console.log({ error });
+        return error;
+    }
 }
 
 // seedSalesReps()
