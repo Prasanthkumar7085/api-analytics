a522882ccb1bcdabd8ae6d7eab934100e3f11596
be-dev1_labsquire.com <be-dev1@labsquire.com>
2024-03-27 15:35:43 +0530

    RS-- Updated facilities sync api

--- src/helpers/syncHelper.ts
+++ src/helpers/syncHelper.ts
@@ -52,11 +52,12 @@ export class syncHelpers {
 
   async getNewSalesRepsData(marketersData) {
     const finalObj = [];
-    const managerIds = [];
 
     const data = marketersData.map((item) => item._id.toString());
 
-    const matchedIdsResult = await db.execute(sql`SELECT ref_id FROM sales_reps WHERE ref_id IN ${data}`);
+    const matchedIdsResult = await db.execute(
+      sql`SELECT ref_id FROM sales_reps WHERE ref_id IN ${data}`,
+    );
 
     const refIdValues = matchedIdsResult.rows.map((obj) => obj.ref_id);
 
@@ -64,11 +65,13 @@ export class syncHelpers {
     const unMatchedIds = data.filter((id) => !refIdValues.includes(id));
 
     if (unMatchedIds.length > 0) {
-      const managerIds = marketersData
+      const marketersIds = marketersData
         .filter((item) => unMatchedIds.includes(item._id.toString()))
         .map((item) => item.reporting_to[0].toString());
 
-      const managerData = await db.execute(sql`SELECT id, ref_id FROM sales_reps WHERE ref_id IN ${managerIds}`);
+      const managerData = await db.execute(
+        sql`SELECT id, ref_id FROM sales_reps WHERE ref_id IN ${marketersIds}`,
+      );
 
       const dataList = marketersData.map((marketer) => {
         let reportingTo = 2; // Default reportingTo value
@@ -96,77 +99,82 @@ export class syncHelpers {
     return finalObj;
   }
 
-
   async getFacilitiesDataFromSalesReps(SalesRepsData) {
-      const salesRepsAndFacilityData = [];
-      const hospitalsData = new Set();
+    const salesRepsAndFacilityData = [];
+    const hospitalsData = new Set();
 
-      // fetching selas rep id and hospitals data from mongo db
-      const data = SalesRepsData.map((item) => ({
-          hospitals: item.hospitals,
-          id: item._id.toString(),
-      }));
+    // fetching selas rep id and hospitals data from mongo db
+    const data = SalesRepsData.map((item) => ({
+      hospitals: item.hospitals,
+      id: item._id.toString(),
+    }));
 
-      data.forEach((item) => {
-          item.hospitals.forEach((hospital) => {
-              const hospitalId = hospital.toString();
-              const itemId = item.id;
-              
-              // Check if the hospital has already been added
-              if (!hospitalsData.has(hospitalId)) {
-                  // stoing sales_reps id and facility id
-                  salesRepsAndFacilityData.push({ 
-                      id: itemId,
-                      hospital: hospitalId,
-                  });
-                  hospitalsData.add(hospitalId);
-              }
+    data.forEach((item) => {
+      item.hospitals.forEach((hospital) => {
+        const hospitalId = hospital.toString();
+        const itemId = item.id;
+
+        // Check if the hospital has already been added
+        if (!hospitalsData.has(hospitalId)) {
+          // stoing sales_reps id and facility id
+          salesRepsAndFacilityData.push({
+            id: itemId,
+            hospital: hospitalId,
           });
+          hospitalsData.add(hospitalId);
+        }
       });
-      const hospitalIds = salesRepsAndFacilityData.map((item) => item.hospital); // fetching hospital id's  
+    });
+    const hospitalIds = salesRepsAndFacilityData.map((item) => item.hospital); // fetching hospital id's
 
-      const salesRepsIds = salesRepsAndFacilityData.map((item) => item.id); // fetching sales reps id's
+    const salesRepsIds = salesRepsAndFacilityData.map((item) => item.id); // fetching sales reps id's
 
-      const salesRepsData = await db.execute(sql`SELECT id, ref_id FROM sales_reps WHERE ref_id IN ${salesRepsIds}`); // fetching sales reps id and ref_id from analytics db
+    const salesRepsData = await db.execute(
+      sql`SELECT id, ref_id FROM sales_reps WHERE ref_id IN ${salesRepsIds}`,
+    ); // fetching sales reps id and ref_id from analytics db
 
-      const matchedIdsResult = await db.execute(sql`SELECT ref_id FROM facilities WHERE ref_id IN ${hospitalIds}`); // fetching matched facilities id from analytics db
+    const matchedIdsResult = await db.execute(
+      sql`SELECT ref_id FROM facilities WHERE ref_id IN ${hospitalIds}`,
+    ); // fetching matched facilities id from analytics db
 
-      const refIdValues = matchedIdsResult.rows.map((obj) => obj.ref_id);
+    const refIdValues = matchedIdsResult.rows.map((obj) => obj.ref_id);
 
-      const unMatchedFacilitiesIds = hospitalIds.filter((id) => !refIdValues.includes(id)); // fetching un-matched id of facilities
+    const unMatchedFacilitiesIds = hospitalIds.filter(
+      (id) => !refIdValues.includes(id),
+    ); // fetching un-matched id of facilities
 
-      // Storing sales reps IDs related to hospital object
-      salesRepsData.rows.forEach((row) => {
-        salesRepsAndFacilityData.find((item) => {
-            if (item.id === row.ref_id.toString()) {
-                item.salesRepsIdsData = row.id;
-            }
-          });
+    // Storing sales reps IDs related to hospital object
+    salesRepsData.rows.forEach((row) => {
+      salesRepsAndFacilityData.find((item) => {
+        if (item.id === row.ref_id.toString()) {
+          item.salesRepsIdsData = row.id;
+        }
       });
+    });
     return { salesRepsAndFacilityData, unMatchedFacilitiesIds };
   }
 
+  async getFacilitiesIds(data, salesRepsAndFacilityData) {
+    const finalObj = [];
 
-  async getFacilitiesIds(data, salesRepsAndFacilityData){
-    
-    const finalObj = []
+    const facilitiesIds = data.map((item) => item._id.toString()); // fetching facilitiec id from hospitals data
 
-    const facilitiesIds = data.map(item => item._id.toString()) // fetching facilitiec id 
-    
-    salesRepsAndFacilityData.forEach(item => {
+    salesRepsAndFacilityData.forEach((item) => {
       if (facilitiesIds.includes(item.hospital)) {
-          // Find the corresponding object in data
-            const matchingObject = data.find(obj => obj._id.toString() === item.hospital);
-            if (matchingObject) {
-                finalObj.push({
-                  name: matchingObject.name,
-                  refId: matchingObject._id.toString(),
-                  salesRepId: item.salesRepsIdsData
-                })
-            }
-          }
-      });
+        // Find the corresponding object in data
+        const matchingObject = data.find(
+          (obj) => obj._id.toString() === item.hospital,
+        );
+        if (matchingObject) {
+          finalObj.push({
+            name: matchingObject.name,
+            refId: matchingObject._id.toString(),
+            salesRepId: item.salesRepsIdsData,
+          });
+        }
+      }
+    });
 
-    return finalObj
+    return finalObj;
   }
 }
