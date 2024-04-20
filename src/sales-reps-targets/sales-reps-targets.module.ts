f05ecdb5b40ffc9e017c27fa986717fa89834646
code.labsquire <code@labsquire.com>
2024-04-20 15:53:17 +0530

    Ta Update salesrep target module

--- src/sales-reps-targets/sales-reps-targets.module.ts
+++ src/sales-reps-targets/sales-reps-targets.module.ts
@@ -7,9 +7,32 @@ import { SESAPIDataServiceProvider } from 'src/notifications/sesAPIDataServicePr
 import { Configuration } from 'src/config/config.service';
 import { ConfigService } from '@nestjs/config';
 import { FilterHelper } from 'src/helpers/filterHelper';
+import { JwtService } from '@nestjs/jwt';
+import { LisService } from 'src/lis/lis.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { insurancePayorsSchema } from 'src/schemas/insurancPayors';
+import { UserSchema } from 'src/schemas/userSchema';
+import { CaseSchema } from 'src/schemas/caseSchema';
+import { testPanelsDataSchema } from 'src/schemas/testPanelSchema';
+import { HospitalSchema } from 'src/schemas/hospitalSchema';
+import { labDataSchema } from 'src/schemas/lab';
+import { MghDbConnections } from 'src/helpers/mghDbconnection';
+import { MghSyncService } from 'src/mgh-sync/mgh-sync.service';
 
 @Module({
   controllers: [SalesRepsTargetsController],
-  providers: [SalesRepsTargetsService, SalesRepService, EmailServiceProvider, SESAPIDataServiceProvider, Configuration, ConfigService, FilterHelper],
+  providers: [SalesRepsTargetsService, SalesRepService, EmailServiceProvider, SESAPIDataServiceProvider, Configuration, ConfigService, FilterHelper, JwtService, LisService, MghDbConnections, MghSyncService],
+  imports: [
+    MongooseModule.forRoot(process.env.LIS_DLW_DB_URL + '&authSource=admin'),
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'Case', schema: CaseSchema },
+      { name: 'Insurance_Payors', schema: insurancePayorsSchema },
+      { name: 'Test_Panels', schema: testPanelsDataSchema },
+      { name: 'Hospital', schema: HospitalSchema },
+      { name: 'Lab', schema: labDataSchema }
+
+    ]),
+  ]
 })
 export class SalesRepsTargetsModule { }
