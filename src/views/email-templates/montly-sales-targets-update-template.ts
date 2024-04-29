89ea478cbb9e5e7414727f60b0c782b7a3db2ed5
code.labsquire <code@labsquire.com>
2024-04-29 10:15:00 +0530

    Update labsquire log with call icon in update target email tempalte

--- src/views/email-templates/montly-sales-targets-update-template.ts
+++ src/views/email-templates/montly-sales-targets-update-template.ts
@@ -94,7 +94,7 @@ export const monthlyTargetsUpdateTemplate = `<!DOCTYPE html>
                     height: 32px;
                     overflow: hidden;
                     flex-shrink: 0;
-                  " alt="" src="${process.env.API_URL}/public/images/CallIcon.png" />
+                  " alt="" src="${process.env.API_URL}/public/images/LabsquireLogo.png" />
 
                             <img style="
                     width: 32px;
